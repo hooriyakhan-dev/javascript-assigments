0dfc5b206cd2a8bca2d6cb1e22f68132894994ee
hooriyakhan-dev <hooriyakhandev@gmail.com>
2026-01-30 02:10:38 +0500

    Add files via upload

---
+++ java-script/chapter-02/script.js
@@ -0,0 +1,44 @@
+// Question 1: Declare a variable called 'username'
+var username = "Hooriya";
+
+// Question 2: Declare a variable called 'myName' and assign your full name
+var myName = "Hooriya Khan";
+
+
+    // Question 3:
+    var message;            // a) Declare variable
+    message = "HELLO WORLD!"; // b) Assign value
+    alert(message);          // c) Display in alert
+
+    // Question 4:
+       // Question 4: Student’s bio data
+    var name = "Hooriya Khan";
+    var age = "18 years old";
+    var course = "web and app development"; 
+
+    alert(name);
+    alert(age);
+    alert(course);
+
+     // Question 5: Display alert using one JS variable
+    var text = "PIZZA\nPIZZ\nPIZ\nPI\nP"; // 
+    alert(text);
+
+     // Question 6: Declare email variable and show message
+    var email = "hooriyakhan-dev@gmail.com";   // store email
+    alert("My email address is " + email);   // show in alert using concatenation
+
+
+     // Question 7: Declare book variable and show alert
+    var book = "A smarter way to learn JavaScript";  // variable declaration & assignment
+    alert("I am trying to learn from the book " + book); // show in alert
+
+
+ // Question 8: Display text in browser
+    var message = "<H1>Yay! I can write in the browser using JavaScript.</H1>"; // store text in variable
+    document.writeln(message); // display in browser
+
+
+    // Question 9: Display text in browser
+    var fancyText = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+    alert(fancyText);
