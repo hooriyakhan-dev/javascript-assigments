5f05ed1dcbb44668406fc1fb4101b031875f24bc
hooriyakhan-dev <hooriyakhandev@gmail.com>
2026-01-31 03:01:50 +0500

    Add files via upload

---
+++ java-script/chapter-04/script.js
@@ -0,0 +1,26 @@
+ // Question 1: Declare 3 variables in one statement
+    var name = "Hooriya", age = 18, country = "Pakistan";
+
+    // Question 2: Legal and Illegal variable names
+    // Legal variable names
+    var myName;
+    var _age;
+    var $price;
+    var firstName;
+    var user_1;
+
+    // Illegal variable names (commented because they will cause error)
+    // var 1name;
+    // var my-name;
+    // var @price;
+    // var first name;
+    // var alert;
+
+    // Question 3: Display rules in browser
+    document.write("<h1>Rules for naming JS variables</h1>");
+    document.write("Variable names can only contain letters, numbers, $, and _.<br>");
+    document.write("For example: $my_1stVariable<br>");
+    document.write("Variables must begin with a letter, $ or _.<br>");
+    document.write("For example: $name, _name or name<br>");
+    document.write("Variable names are case sensitive.<br>");
+    document.write("Variable names should not be JS keywords.<br>");
